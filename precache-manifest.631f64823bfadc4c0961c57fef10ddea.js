74c40615e831acc63651fa55ee6cc17c06014faa
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-12-31 12:07:59 +0300

    Updates

---
+++ precache-manifest.631f64823bfadc4c0961c57fef10ddea.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "e5bb4bc60a105e446435",
+    "url": "/react/static/js/main.e5bb4bc6.chunk.js"
+  },
+  {
+    "revision": "ef487f788d4f16497d67",
+    "url": "/react/static/js/1.ef487f78.chunk.js"
+  },
+  {
+    "revision": "f8702a7c6465fe4bfd13ac2eacab7f7a",
+    "url": "/react/index.html"
+  }
+];
